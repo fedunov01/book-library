6e98546d71d17de3784b105c173705e59b2cd1b6
Fedunov Aleksei <fedunov@re-view.co.jp>
2026-08-07 15:32:18 +0900

    week01 task02

---
+++ week-01/tasks/task02.php
@@ -0,0 +1,20 @@
+<?php
+
+$x = (int) readline(" >> ");
+
+if ($x % 2 == 0) {
+    echo "偶数" . "\n";
+} else {
+    echo "奇数" . "\n";
+}
+
+for ($i = 1; $i <= 20; $i++) {
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "$i: FizzBuzz" . "\n";
+    } else if ($i % 3 == 0){
+        echo "$i: Fizz" . "\n";
+    } else if ($i % 5 == 0){
+        echo "$i: Buzz" . "\n";
+    }
+    
+}
