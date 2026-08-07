6e915f878474e6277690e1e6562cf05920c290b1
Fedunov Aleksei <fedunov@re-view.co.jp>
2026-08-07 15:49:36 +0900

    week01 task03

---
+++ week-01/tasks/task03.php
@@ -0,0 +1,17 @@
+<?php
+
+$array = [1, 12, 3, 14, 5, 16, 7, 18, 9, 10];
+
+function normaliser($str, $age) {
+    $str = strtolower($str);
+    $str = $str . " " . $age;
+    $str = str_replace(" ", "_", $str);
+    return $str;
+}
+
+
+
+echo normaliser("John Doe", 15) . "\n"; 
+echo implode(", ", array_filter($array, function ($array) {
+    return $array > 10 ;
+}));
